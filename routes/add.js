db10dc9e0b5aa778a05be8f81f39fc1ba04165ab
troyany2016 <troyany2016@gmail.com>
2019-08-06 15:09:01 +0300

    simple model, save to file

--- routes/add.js
+++ routes/add.js
@@ -1,4 +1,5 @@
 const {Router} = require('express');
+const Course = require('../models/courses');
 
 const router = Router();
 
@@ -6,10 +7,12 @@ router.get('/', (req, res) => {
 	res.render('add', {
 		title: 'Add new course'
 	})
-})
+});
 
-router.post('/', (req, res) => {
-	console.log(req.body)
+router.post('/', async (req, res) => {
+
+	const course = new Course(req.body.title, req.body.price, req.body.img);
+	await course.save();
 
 	res.redirect('/courses')
 })
