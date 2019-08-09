5a2f1fac19de856878c8f69c571ecbf836847fc8
troyany2016 <troyany2016@gmail.com>
2019-08-09 11:09:24 +0300

    login real user

--- routes/auth.js
+++ routes/auth.js
@@ -18,14 +18,33 @@ router.get('/logout', async (req, res) => {
 })
 
 router.post('/login', async (req, res) => {
-	const user =  await User.findById('5d4be3eededfb917f0094b69');
-	req.session.user  = user
-	req.session.isAuthenticated = true;
-	req.session.save((err) => {
-		if (err) throw err;
+	try {
+		const {email, password} = req.body;
+		const candidate = await User.findOne({email})
 
-		res.redirect('/')
-	})
+		if(candidate){
+			const areSame = password === candidate.password;
+			if(areSame){
+
+				req.session.user  = candidate
+				req.session.isAuthenticated = true;
+				req.session.save((err) => {
+					if (err) throw err;
+
+					res.redirect('/')
+				})
+
+			}else{
+				res.redirect('/auth/login')
+			}
+		}else{
+			res.redirect('/auth/login')
+		}
+
+	}catch (e) {
+		console.log(e)
+	}
+
 
 })
 
