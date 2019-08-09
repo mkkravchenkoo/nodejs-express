0e33db4d15e760a1ffe471e0071f7933ae87dee6
troyany2016 <troyany2016@gmail.com>
2019-08-09 16:05:43 +0300

    only owner can change own course

--- routes/courses.js
+++ routes/courses.js
@@ -4,26 +4,50 @@ const auth  = require('../middleware/auth');
 
 const router = Router();
 
-router.get('/', async (req, res) => {
-	const courses = await Courses.find()
-		.populate('userId', 'email name')
-		.select('img price title');
 
-	res.render('courses', {
-		title: 'All courses',
-		courses
-	})
+function isOwner(course, req){
+	return course.userId.toString() === req.user._id.toString()
+}
+
+router.get('/', async (req, res) => {
+	try {
+		const courses = await Courses.find()
+			.populate('userId', 'email name')
+			.select('img price title');
+
+		res.render('courses', {
+			title: 'All courses',
+			courses,
+			userId:req.user ? req.user._id.toString() : null
+		})
+	}catch (e) {
+		console.log(e)
+	}
+
 })
 
 router.get('/:id/edit', auth, async (req, res) => {
 	if(!req.query.allow){
 		return res.redirect('/')
 	}
-	const course = await Courses.findById(req.params.id);
-	res.render('course-edit', {
-		title: course.title,
-		course,
-	})
+
+	try {
+
+		const course = await Courses.findById(req.params.id);
+
+		if(!isOwner(course, req)){
+			return res.redirect('/courses')
+		}
+
+		res.render('course-edit', {
+			title: course.title,
+			course,
+		})
+
+	}catch (e) {
+		console.log(e)
+	}
+
 
 });
 
@@ -38,11 +62,29 @@ router.get('/:id', auth, async (req, res) => {
 })
 
 router.post('/edit', auth, async (req, res) => {
-	const {id} = req.body;
-	delete req.body.id;
 
-	await Courses.findByIdAndUpdate(id, req.body);
-	res.redirect('/courses')
+	try {
+
+		const {id} = req.body;
+		delete req.body.id;
+
+		const course = await Courses.findById(id)
+
+		if(!isOwner(course, req)){
+			return res.redirect('/courses')
+		}
+
+		Object.assign(course, req.body)
+		await course.save();
+
+		// await Courses.findByIdAndUpdate(id, req.body);
+		res.redirect('/courses')
+
+	}catch (e) {
+		console.log(e)
+	}
+
+
 });
 
 
---
+++ utils/hbs-helpers.js
@@ -0,0 +1,10 @@
+module.exports = {
+	ifeq(a, b, options){
+
+		if(a == b){
+			return options.fn(this)
+		}
+
+		return options.inverse(this)
+	}
+}
