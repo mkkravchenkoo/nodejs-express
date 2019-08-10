707a700a792bf003d2f2fbea147b3c9b8d4948b4
Nikolay Kravchenko <mkkravchenkoo@gmail.com>
2019-08-10 11:15:56 +0300

    Sanitizers

--- utils/validators.js
+++ utils/validators.js
@@ -14,13 +14,13 @@ exports.registerValidators = [
 		}catch (e) {
 			console.log(e)
 		}
-	}),
+	}).normalizeEmail(),
 	body('password', 'Password must be 6-56 symbols with alphanumeric').isLength({min:6, max:56}).isAlphanumeric(),
 	body('confirm').custom((value, {req}) => {
 		if(value !== req.body.password){
 			throw new Error('Password doesn"t matched!')
 		}
 		return true
-	}),
-	body('name').isLength({min:3}).withMessage('Name must be more than 3 symbols')
+	}).trim(),
+	body('name').isLength({min:3}).withMessage('Name must be more than 3 symbols').trim()
 ]
